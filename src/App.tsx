3659f61b5099ec377967409819fe539dc97fc01d
BenCauffman <benjamincauffman@gmail.com>
2023-01-19 09:55:59 -0700

    animation kinda working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { useState } from "react";
 import MainBody from "./MainBody";
 import ProfileLayout from "./ProfileLayout";
 import DetailContext from "./context";
 
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#F5f5f5',
+      200: '#EEEEEE',
+      300: '#E0E0E0',
+      400: '#BDBDBD',
+      500: '#9E9E9E',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none',
+          },
+        },
+      },
+    },
+  },
+})
 
 const App: React.FC = () => {
   const [active, setActive] = useState('About Me');
@@ -11,7 +38,7 @@ const App: React.FC = () => {
   return (
     <div>
       <DetailContext.Provider value={{active, setActive, section, setSection}}>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
           <ProfileLayout />
           <MainBody />
         </ChakraProvider>
--- src/ProfileLayout.tsx
+++ src/ProfileLayout.tsx
@@ -1,41 +1,120 @@
 import { Box, interactivity, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import DetailParent from "./DetailParent";
+import { useEffect, useState, useMemo, lazy, Suspense } from "react";
+import space from "../public/images/space.png";
+import { useTransition, animated, to } from "react-spring";
 
-const TITLES = [
-  "Full Stack Engineer",
-  "Cognitive Scientist",
-  "League of Legends Enjoyer",
+const DetailParent = lazy(() => import("./DetailParent"));
+type title = {
+  title: string;
+  x: number;
+  y: number;
+};
+
+const TITLES: title[] = [
+  {
+    title: "Web Developer",
+    x: -20,
+    y: -200,
+  },
+  {
+    title: "League of Legends Enjoyer",
+    x: 180,
+    y: 100,
+  },
+  {
+    title: "Comedy Peruser",
+    x: 380,
+    y: -200,
+  },
 ];
 
-
 const ProfileLayout: React.FC = () => {
-  const [index, setIndex] = useState(0);
+  const original: title[] = useMemo(() => {
+    console.log("hi")
+    return JSON.parse(JSON.stringify(TITLES));
+  }, [TITLES]);
+  const [rows, setRows] = useState(TITLES);
+
+  const rotateItems = (list: title[]) => {
+    list.unshift(list.pop()!);
+    return setPosition(list);
+  };
+
+  const setPosition = (list: title[]) => {
+    return list.map((item, index) => {
+      item.x = original[index].x;
+      item.y = original[index].y;
+      return item;
+    });
+  };
+
+  const transitions = useTransition(rows, {
+    key: (item: { title: string; x: number; y: number }) => item.title,
+    enter: ({ x, y }) => ({ x, y }),
+    update: ({ x, y }) => ({ x, y }),
+    config: { duration: 2500 },
+  });
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex((prevIndex) =>
-        prevIndex === TITLES.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 5000);
+      setRows(rotateItems(rows));
+    }, 2500);
     return () => clearInterval(interval);
   });
+
   return (
     <>
       <Box
         textAlign="center"
-        bg="tomato"
-        // position="fixed"
-        // top="0"
-        // h="200px"
-        // w="100vw"
+        bg="black"
+        h="100vh"
+        w="100vw"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
       >
-        <Text fontSize="5xl">Benjamin Cauffman</Text>
-        <Text fontSize="3xl">{TITLES[index]}</Text>
+        <Box position="relative">
+          <Text color="white" fontSize="5xl">
+            Ben Cauffman
+          </Text>
+          {transitions(({ x, y }, item) => {
+            if (item.title !== "") {
+              return (
+                <animated.div
+                  style={{
+                    transform: to(
+                      [x, y],
+                      (x, y) => `translate(${x}px, ${y}px)`
+                    ),
+                    backgroundColor: "teal",
+                    position: "absolute",
+                    fontSize: "20px",
+                    color: "white",
+                  }}
+                >
+                  {item.title}
+                </animated.div>
+              );
+            } else {
+              return (
+                <animated.div
+                  style={{
+                    transform: to(
+                      [x, y],
+                      (x, y) => `translate(${x}px, ${y}px)`
+                    ),
+                  }}
+                ></animated.div>
+              );
+            }
+          })}
+        </Box>
       </Box>
+      <Suspense>
       <Box position="relative" bg="yellow">
         <DetailParent />
       </Box>
+      </Suspense>
     </>
   );
 };
